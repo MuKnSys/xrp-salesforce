383277ba8b5cacc454c063a57b30fe92eed246b9
mbedlinski <mbedlinski@aquivalabs.com>
2024-01-12 15:33:43 +0100

    Retrieval of settings

--- force-app/main/default/lwc/setup/setup.js
+++ force-app/main/default/lwc/setup/setup.js
@@ -1,15 +1,37 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
-import save from "@salesforce/apex/SetupCtrl.save";
+import getSettings from "@salesforce/apex/SetupCtrl.getSettings";
+import saveSettings from "@salesforce/apex/SetupCtrl.saveSettings";
 
 import { labels } from './setupLabels';
 
 export default class Setup extends LightningElement {
     apiKey = null;
     apiSecret = null;
+    @track placeholders = {};
 
     labels = labels;
+
+    async processSettings() {
+        const settings = await getSettings();
+        if(settings.apiKey) {
+            this.placeholders['apiKey'] = 'API Key is setted up';
+        }
+        if(settings.apiSecret) {
+            this.placeholders['apiSecret'] = 'API Secret is setted up';
+        }
+    }
+
+    showToastNotification(message, title=labels.errorTitle, variant='error') {
+        const toast = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant,
+        });
+
+        this.dispatchEvent(toast);
+    }
 
     handleApiKey(event) {
         this.apiKey = event.target.value;
@@ -25,19 +47,13 @@ export default class Setup extends LightningElement {
             return;
         }
 
-        const result = await save({
+        await saveSettings({
             apiKey : this.apiKey,
             apiSecret : this.apiSecret
         });
     }
 
-    showToastNotification(message, title=labels.errorTitle, variant='error') {
-        const toast = new ShowToastEvent({
-            title: title,
-            message: message,
-            variant: variant,
-        });
-
-        this.dispatchEvent(toast);
+    async connectedCallback() {
+        await this.processSettings();
     }
 }
