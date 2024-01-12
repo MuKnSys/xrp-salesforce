03d04c2515970a5d08a105cde0382ba1b3bb3367
mbedlinski <mbedlinski@aquivalabs.com>
2024-01-12 13:45:01 +0100

    Credentials Setup init

--- force-app/main/default/lwc/setup/setup.js
+++ force-app/main/default/lwc/setup/setup.js
@@ -1,3 +1,38 @@
 import { LightningElement } from 'lwc';
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
-export default class Setup extends LightningElement {}
+import { labels } from './setupLabels';
+
+export default class Setup extends LightningElement {
+    apiKey = null;
+    apiSecret = null;
+
+    labels = labels;
+
+    handleApiKey(event) {
+        this.apiKey = event.target.value;
+    }
+
+    handleApiSecret(event) {
+        this.apiSecret = event.target.value;
+    }
+
+    handleSave() {
+        if(this.apiKey == null || this.apiSecret == null){
+            this.showToastNotification(labels.errorMessage);
+            return;
+        }
+
+        // Call apex
+    }
+
+    showToastNotification(message, title=labels.errorTitle, variant='error') {
+        const toast = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant,
+        });
+
+        this.dispatchEvent(toast);
+    }
+}
